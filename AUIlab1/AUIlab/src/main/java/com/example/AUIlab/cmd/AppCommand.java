c3a38c4c023c259b1a086b69fb6438a793866f06
Kuba Lisowski <123380675+KubaBubaB@users.noreply.github.com>
2023-10-22 01:49:41 +0200

    Done, note to self: Ask why database tables are not visable(not linked with JPA and repositories?)

--- AUIlab1/AUIlab/src/main/java/com/example/AUIlab/cmd/AppCommand.java
+++ AUIlab1/AUIlab/src/main/java/com/example/AUIlab/cmd/AppCommand.java
@@ -1,12 +1,13 @@
 package com.example.AUIlab.cmd;
 
 import com.example.AUIlab.EntityClasses.Job;
+import com.example.AUIlab.EntityClasses.Person;
 import com.example.AUIlab.Services.JobService;
 import com.example.AUIlab.Services.PersonService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Scanner;
+import java.util.*;
 
 @Component
 public class AppCommand implements CommandLineRunner {
@@ -27,15 +28,50 @@ public class AppCommand implements CommandLineRunner {
             switch (command){
                 case "add_person" ->{
                     //add a single person
+                    System.out.println("UUID of person to be added:");
+                    UUID uuidPerson = UUID.fromString(scanner.next());
+                    System.out.println("Name of person to be added:");
+                    String name = scanner.next();
+                    System.out.println("Salary of person to be added:");
+                    int salary;
+                    try{
+                        salary = scanner.nextInt();
+                    }
+                    catch(InputMismatchException ex){
+                        System.out.println("Not an integer");
+                        continue;
+                    }
+
+                    System.out.println("Job UUID of person to be added:");
+                    UUID uuidJob = UUID.fromString(scanner.next());
+                    try {
+                        personService.create(Person.builder().
+                                id(uuidPerson).name(name).salary(salary).
+                                job(jobService.find(uuidJob).orElseThrow()).build());
+                    } catch (IllegalArgumentException ex) {
+                        System.out.println("Bad Request");
+                    }
+
                 }
                 case "delete_person" ->{
                     //delete person by uuid
+                    System.out.println("UUID of person to be deleted:");
+                    try {
+                        UUID uuid = UUID.fromString(scanner.next());
+                        personService.delete(uuid);
+                    } catch (NoSuchElementException ex) {
+                        System.out.println("Not Found");
+                    }
+
+
                 }
                 case "list_all_people" ->{
                     //list all people
+                    personService.findAll().forEach(System.out::println);
                 }
                 case "list_all_jobs" ->{
                     //list jobs
+                    jobService.findAll().forEach(System.out::println);
                 }
                 case "help" ->{
                     System.out.println("""
